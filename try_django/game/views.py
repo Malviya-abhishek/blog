4b1b354b29045db8b2a26cc11216f6dec5d7d24e
Abhishek Malviya <abhismalviya@outlook.com>
2020-10-25 23:02:44 +0530

    many changes

--- try_django/game/views.py
+++ try_django/game/views.py
@@ -3,17 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def game_list_view(request):
-    template = "game\index.html"
-    context = {"game1":"game\index.html"}
+    template = "game/index.html"
+    context = {"game1":"game/index.html"}
     return render(request,template,context)
 
 def game_detail_view(request):
-    template = "game\list.html"
+    template = "game/list.html"
     context = {"game1":"index.html"}
     return render(request,template,context)
 
 
 def js_view(request):
-    template = "game\script.js"
+    template = "game/script.js"
     context = {}
     return render(request,template,context)
